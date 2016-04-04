40af1f3d0c75ec575ce26bf6319d1ab32820dca9
Tobias Gozzi <tobiasgozzi@Tobiass-iMac.local>
2016-04-04 15:59:01 +0200

    added third string as println-argument

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String text = "Hello";
+        String text2 = "World";
+        
+        System.out.println(text+" "+text2+" in new branch");
+        
+    }
+    
+}
